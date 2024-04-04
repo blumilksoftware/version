d81cff6f6c24573c96173e8267a49d4d0c25a2e3
Kamil <kamil.piech.97@gmail.com>
2024-04-04 10:08:32 +0200

    #1 - added simple setup of package with tests, codestyle and environment stuff

---
+++ Makefile
@@ -0,0 +1,25 @@
+DOCKER_COMPOSE_FILENAME=docker-compose.yml
+PHP_FPM_SERVICE_NAME=php
+
+.PHONY: shell
+shell:
+	docker compose -f ${DOCKER_COMPOSE_FILENAME} exec ${PHP_FPM_SERVICE_NAME} ash
+
+.PHONY: test
+test:
+	docker compose -f ${DOCKER_COMPOSE_FILENAME} exec ${PHP_FPM_SERVICE_NAME} composer test
+
+.PHONY: csf
+csf:
+	docker compose -f ${DOCKER_COMPOSE_FILENAME} exec ${PHP_FPM_SERVICE_NAME} composer csf
+
+.PHONY: run
+run:
+	docker compose -f ${DOCKER_COMPOSE_FILENAME} up -d
+
+.PHONY: stop
+stop:
+	docker compose -f ${DOCKER_COMPOSE_FILENAME} stop
+
+.PHONY: restart
+restart: stop run
---
+++ tests/GenerateVersionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Blumilk\Version\VersionHelper;
+use PHPUnit\Framework\TestCase;
+
+class GenerateVersionTest extends TestCase
+{
+    public function testGeneratingVersion(): void
+    {
+        $version = (new VersionHelper())->generate();
+
+        $this->assertIsString($version);
+        $this->assertStringNotContainsString("|", $version);
+    }
+
+    public function testGeneratingLongVersion(): void
+    {
+        $version = (new VersionHelper(true))->generate();
+        $versionHash = trim(shell_exec("git log --format='%h' -n 1"));
+
+        $this->assertIsString($version);
+        $this->assertStringContainsString("|", $version);
+        $this->assertStringContainsString($versionHash, $version);
+    }
+}
